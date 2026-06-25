75c199eac9e5ecc364381a0a514f3b92a6c2ed7a
Reliantstrategies <reliantstrategies@yahoo.com>
2026-06-24 21:55:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const revealEls = document.querySelectorAll('.reveal');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) entry.target.classList.add('show');
+  });
+}, { threshold: 0.12 });
+revealEls.forEach((el) => observer.observe(el));
