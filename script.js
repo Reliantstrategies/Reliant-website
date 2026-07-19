8057f1c13c71bdb0a05ef35e63852ad32a58ece4
Reliantstrategies <reliantstrategies@yahoo.com>
2026-07-19 16:27:41 -0700

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const reveals=document.querySelectorAll('.reveal');const observer=new IntersectionObserver((entries)=>{entries.forEach(entry=>{if(entry.isIntersecting)entry.target.classList.add('show')})},{threshold:.12});reveals.forEach(el=>observer.observe(el));
